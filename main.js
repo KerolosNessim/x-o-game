33774f311c6ff48aeab982b84e2fb2a231039c05
KerolosNessim <Keronessim9@gmail.com>
2024-04-09 22:37:07 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+
+let tittle =document.querySelector(".tittle")
+let turn = "x";
+let squares = [];
+function end(num1, num2, num3)
+{
+  tittle.innerHTML = `<span>${squares[num1]}</span> winner`;
+  document.getElementById("item" + num1).style.background = " rgb(11, 177, 78)";
+  document.getElementById("item" + num2).style.background = " rgb(11, 177, 78)";
+  document.getElementById("item" + num3).style.background = " rgb(11, 177, 78)";
+
+  setInterval(function () {
+  tittle.innerHTML += ".";
+  }, 1000)
+  setTimeout(function () {
+  location.reload();
+  },4000)
+}
+
+function winner() {
+  for (let i = 1; i < 10; i++) {
+    squares[i] = document.getElementById("item" + i).innerHTML;
+  }
+  if (squares[1] == squares[2] && squares[2] == squares[3] && squares[1] != "") {
+    end(1, 2, 3);
+  }
+  else if (squares[4] == squares[5] && squares[5] == squares[6] && squares[4] != "") {
+    end(4, 5, 6);
+  }
+  else if (squares[7] == squares[8] && squares[8] == squares[9] && squares[7] != "") {
+    end(7, 8, 9);
+  }
+  else if (squares[1] == squares[4] && squares[4] == squares[7] && squares[1] != "") {
+    end(1, 4, 7);
+  }
+  else if (squares[2] == squares[5] && squares[5] == squares[8] && squares[2] != "") {
+    end(2, 5, 8);
+  }
+  else if (squares[3] == squares[6] && squares[6] == squares[9] && squares[3] != "") {
+    end(3, 6, 9);
+  }
+  else if (squares[1] == squares[5] && squares[5] == squares[9] && squares[1] != "") {
+    end(1, 5, 9);
+  }
+  else if (squares[3] == squares[5] && squares[5] == squares[7] && squares[3] != "") {
+    end(3, 5, 7);
+  }
+}
+
+function game(id)
+{
+  let element = document.getElementById(id);
+  if (turn === "x" && element.innerHTML == "")
+  {
+    element.innerHTML = "x";
+    turn = "o"
+    tittle.innerHTML="turn <span>o</span>"
+  }
+  else if (turn === "o" && element.innerHTML == "")
+  {
+    element.innerHTML = "o";
+    turn = "x"
+    tittle.innerHTML="turn <span>x</span>"
+  }
+  winner();
+}
